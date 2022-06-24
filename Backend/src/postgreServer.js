a11a8a57ca2cd6142df2e3f33b6fbc6022f7ff9c
BOTShiZJ <zhijian_shi@163.com>
2022-06-24 10:17:02 +0800

    db credentials updated

--- Backend/src/postgreServer.js
+++ Backend/src/postgreServer.js
@@ -2,10 +2,10 @@ import pg from "pg"
 const { Pool } = pg
 
 export const pool = new Pool({
-	user: 'nus',
+	user: 'admin',
 	host: 'localhost',
-	database: 'nustesting',
-	password: '12345678',
+	database: 'nuswap',
+	password: 'ncepassword',
 	port: 5432
 })
 
